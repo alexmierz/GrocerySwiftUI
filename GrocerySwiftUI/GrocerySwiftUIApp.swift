d7e651e1313542c21e781761e9a5858f44aa11a2
reassign <snowboardz125@gmail.com>
2023-08-16 15:37:59 -0400

    Initial Commit

---
+++ GrocerySwiftUI/GrocerySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrocerySwiftUIApp.swift
+//  GrocerySwiftUI
+//
+//  Created by Alex Mierzejewski on 8/16/23.
+//
+
+import SwiftUI
+
+@main
+struct GrocerySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
